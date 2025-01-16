8005b43a6ebeeb1b91205fafcd6d8b8903bad30e
VictorNaniii <victor.nani@iis.utm.md>
2025-01-17 00:03:54 +0200

    feat(genere): implement CRUD operations and search functionality in genere module

--- src/genere/genere.controller.ts
+++ src/genere/genere.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { GenereService } from './genere.service';
+import { Auth } from 'src/auth/decorators/aut.decorator';
+import { IdValidationPipe } from 'src/pipes/id.validation.pipes';
+import { CreateGenereDto } from './dto/create.dto.gener';
 
-@Controller('genere')
-export class GenereController {}
+@Controller('generes')
+export class GenereController {
+  constructor(private readonly genereService: GenereService) {}
+
+  @Get('by-slug/:slug')
+  async bySlug(@Param('slug') slug: string) {
+    return this.genereService.bySlug(slug);
+  }
+
+  @Get('collections')
+  async getCollection() {
+    return this.genereService.getColections();
+  }
+  @Get()
+  async getAll(@Query('searchTearms') searchTearms?: string) {
+    return this.genereService.getAllGenere(searchTearms);
+  }
+  @Get('id')
+  @Auth('admin')
+  async getGeners(@Param('id', IdValidationPipe) id: string) {
+    return this.genereService.byId(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async updateGeners(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() dto: CreateGenereDto,
+  ) {
+    return this.genereService.update(id, dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @HttpCode(200)
+  @Auth('admin')
+  async create() {
+    return this.genereService.create();
+  }
+}
--- src/genere/genere.service.ts
+++ src/genere/genere.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { GenerModule } from './genere.model';
 import { ModelType } from '@typegoose/typegoose/lib/types';
+import { CreateGenereDto } from './dto/create.dto.gener';
+import { genSalt } from 'bcryptjs';
 
 @Injectable()
 export class GenereService {
@@ -10,13 +12,66 @@ export class GenereService {
     private readonly GenerModule: ModelType<GenerModule>,
   ) {}
 
+  async getAllGenere(searchTerm?: string) {
+    let option = {};
+
+    if (searchTerm)
+      option = {
+        $or: [
+          {
+            name: new RegExp(searchTerm, 'i'),
+          },
+          {
+            slug: new RegExp(searchTerm, 'i'),
+          },
+          {
+            description: new RegExp(searchTerm, 'i'),
+          },
+        ],
+      };
+
+    return this.GenerModule.find(option)
+      .select('-updatedAt -__v')
+      .sort({ createAt: 'desc' })
+      .exec();
+  }
+
+  async bySlug(slug: string) {
+    return this.GenerModule.findOne({ slug }).exec();
+  }
+
+  async getColections() {
+    const geners = await this.getAllGenere();
+    const collections = geners;
+    // NED WIL WRITE -!!
+    return collections;
+  }
+
+  // --Admin Place
+  async update(id: string, dto: CreateGenereDto) {
+    return this.GenerModule.findByIdAndUpdate(id, dto, {
+      new: true,
+    }).exec();
+  }
+
+  async delete(id: string) {
+    return this.GenerModule.findByIdAndDelete(id).exec();
+  }
+
   async byId(_id: string) {
     const gener = await this.GenerModule.findById(_id);
     if (!gener) throw new NotFoundException('Genere not found');
     return gener;
   }
 
-  async update(id: string, dto: UpdateGenereDto) {
-    
+  async create() {
+    const defaultValue: CreateGenereDto = {
+      descripton: '',
+      icon: '',
+      name: '',
+      slug: '',
+    };
+    const genere = await this.GenerModule.create(defaultValue);
+    return genere._id;
   }
 }

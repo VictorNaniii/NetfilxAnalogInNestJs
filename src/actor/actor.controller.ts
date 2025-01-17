d364dc035335e337beeecc12003a0b0eee169124
VictorNaniii <victor.nani@iis.utm.md>
2025-01-17 13:43:22 +0200

    feat(actor): implement CRUD operations and search functionality in actor module

--- src/actor/actor.controller.ts
+++ src/actor/actor.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ActorModel } from './actor.model';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { ActorService } from './actor.service';
+import { IdValidationPipe } from 'src/pipes/id.validation.pipes';
+import { Auth } from 'src/auth/decorators/aut.decorator';
+import { ActorDto } from './actor.dto';
 
 @Controller('actor')
-export class ActorController {}
+export class ActorController {
+  constructor(private readonly actorService: ActorService) {}
+
+  @Get('by-slug/:slug')
+  async bySlug(@Param('slug') slug: string) {
+    return this.actorService.bySlug(slug);
+  }
+
+  @Get()
+  async getAll(@Query('searchTearms') searchTearms?: string) {
+    return this.actorService.getAll(searchTearms);
+  }
+  @Get(':id')
+  @Auth('admin')
+  async get(@Param('id', IdValidationPipe) id: string) {
+    return this.actorService.byId(id);
+  }
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @HttpCode(200)
+  @Auth()
+  async create() {
+    return this.actorService.create();
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async update(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() dto: ActorDto,
+  ) {
+    return this.actorService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async delete(@Param('id', IdValidationPipe) id: string) {
+    return this.actorService.delete(id);
+  }
+}
--- src/actor/actor.service.ts
+++ src/actor/actor.service.ts
@@ -1,14 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ActorModel } from './actor.model';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { InjectModel } from '@nestjs/mongoose';
+import { NotFoundError, retry } from 'rxjs';
+import { ActorDto } from './actor.dto';
 
 @Injectable()
 export class ActorService {
   constructor(
     @InjectModel(ActorModel.name)
-    private readonly ActorModel: ModelType<ActorModel>, ASDFG5H6K//.,MNUBVTCEXW\
+    private readonly ActorModel: ModelType<ActorModel>,
   ) {}
 
+  async bySlug(slug: string) {
+    return this.ActorModel.findOne({ slug }).exec();
+  }
 
+  async getAll(searchTerm?: string) {
+    let options = {};
+
+    if (searchTerm) {
+      options = {
+        $or: [
+          {
+            name: new RegExp(searchTerm, 'i'),
+          },
+          {
+            slug: new RegExp(searchTerm, 'i'),
+          },
+        ],
+      };
+    }
+    // Ned wil add agregation aftert creating movie model -!!
+
+    return this.ActorModel.find(options)
+      .select('-updateAt -__v')
+      .sort({ createdAt: 'desc' })
+      .exec();
+  }
+  async byId(id: string) {
+    const actor = await this.ActorModel.findById(id);
+    if (!actor) throw new NotFoundException('Actor not found');
+    return actor;
+  }
+
+  async create() {
+    const defaultValue: ActorDto = {
+      name: '',
+      slug: '',
+      photo: '',
+    };
+
+    const actor = await this.ActorModel.create(defaultValue);
+    return actor._id;
+  }
+
+  async update(id: string, dto: ActorDto) {
+    const updateDto = await this.ActorModel.findByIdAndUpdate(id, dto, {
+      new: true,
+    });
+
+    if (!updateDto) throw new NotFoundException('Autor not found');
+
+    return updateDto;
+  }
+
+  async delete(id: string) {
+    const deleteActor = await this.ActorModel.findByIdAndDelete(id).exec;
+
+    if (!deleteActor) throw new NotFoundException('Author not found');
+    return deleteActor;
+  }
+  ///--=
 }
